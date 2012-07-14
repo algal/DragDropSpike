4421b3fdd8f3e09fab09cde49e225b78c48c3214
Alexis Gallagher <alexis@alexisgallagher.com>
2012-07-14 17:13:14 +0100

    added DnD Protocol

--- DragDropSpike/MCKDragDropProtocol.h
+++ DragDropSpike/MCKDragDropProtocol.h
@@ -1,31 +1,28 @@
 //
-//  ViewController.h
-//  DragDropSpike
+//  MCKDragDropProtocol.h
+//  discoveryboard
 //
 //  Created by Alexis Gallagher on 2012-07-14.
-//  Copyright (c) 2012 McKinsey. All rights reserved.
+//
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UIView *leftContainer;
-@property (weak, nonatomic) IBOutlet UIView *rightContainer;
-@property (weak, nonatomic) IBOutlet UIButton *draggableButton;
+//
+// delegate-based
+//
 
-#pragma mark  MCKDnDDonorProtocol delegate
-
+@protocol MCKDnDDonorProtocol <NSObject>
 // when view is picked up
 -(void) donorView:(UIView*)donor didBeginDraggingView:(UIView*)draggingSubview;
 // if view is absorbed by dropped-on view.
 -(void) donorView:(UIView*)donor willDonateDraggingView:(UIView*)draggingSubview;
 -(void) donorView:(UIView*)donor didDonateDraggingView:(UIView*)draggingSubview;
 // if view is rejected by the dropped-on view
--(void) donorView:(UIView*)donor didReclaimDraggingView:(UIView*)draggingSubview;
+-(void) donorView:(UIView*)donor didReclaimDraggingView:(UIView*)draggingSubview
+@end
 
-#pragma mark MCKDnDAbsorberProtocol delegate
-
+@protocol MCKDnDAbsorberProtocol <NSObject>
 -(BOOL) absorberView:(UIView*)absorber canAbsorbDraggingView:(UIView*)draggingSubview;
 -(void) absorberView:(UIView*)absorber absorbDraggingView:(UIView*)draggingSubview;
-
 @end
